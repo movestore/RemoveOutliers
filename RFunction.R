a0c63ea7e45712ca404594094e46589c1dc74e89
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2020-06-30 12:25:19 +0200

    Create RFunction.R
    first R-code for removing (in Movebank manually marked) outliers and positions with impossible speeds.

---
+++ RFunction.R
@@ -0,0 +1,19 @@
+library('move')
+library('foreach')
+
+rFunction <- function(data, maxspeed=NULL)
+{
+  if (is.null(maxspeed)) logger.info("No maximum speed provided, only manually marked Outliers removed.") else logger.info(pasteo("Remove manually marked outliers and positions with maximum speed > ", maxspeed,"m/s"))
+  
+  data.split <- split(data)
+  clean <- foreach(datai = data.split) %do% {
+    logger.info(namesIndiv(datai))
+    datai <- datai[(is.na(datai@data$manually_marked_outlier) | datai@data$manually_marked_outlier==""),]
+    if (!is.null(maxspeed)) datai[speed(datai)<maxspeed,] else datai
+  }
+  names (clean) <- names(data.split)
+  
+  clean_nozero <- clean[unlist(lapply(clean, length) > 1)] #remove list elements of length 1 or 0
+  moveStack(clean_nozero)
+}
+  
