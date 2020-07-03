10e7bd9aefd8b398dba90443c7cff04848cf8c6a
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2020-07-03 10:45:08 +0200

    move::split and typo

--- RFunction.R
+++ RFunction.R
@@ -3,15 +3,15 @@ library('foreach')
 
 rFunction <- function(data, maxspeed=NULL)
 {
-  if (is.null(maxspeed)) logger.info("No maximum speed provided, only manually marked Outliers removed.") else logger.info(pasteo("Remove manually marked outliers and positions with maximum speed > ", maxspeed,"m/s"))
+  if (is.null(maxspeed)) logger.info("No maximum speed provided, only manually marked Outliers removed.") else logger.info(paste0("Remove manually marked outliers and positions with maximum speed > ", maxspeed,"m/s"))
   
-  data.split <- split(data)
+  data.split <- move::split(data)
   clean <- foreach(datai = data.split) %do% {
     logger.info(namesIndiv(datai))
     datai <- datai[(is.na(datai@data$manually_marked_outlier) | datai@data$manually_marked_outlier==""),]
     if (!is.null(maxspeed)) datai[speed(datai)<maxspeed,] else datai
   }
-  names (clean) <- names(data.split)
+  names(clean) <- names(data.split)
   
   clean_nozero <- clean[unlist(lapply(clean, length) > 1)] #remove list elements of length 1 or 0
   moveStack(clean_nozero)
