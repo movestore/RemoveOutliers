accc401a4975092faae07cf5b035a3846dcadf4b
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2020-07-23 13:56:02 +0200

    add visible removal option
    adds possibility to chose if want to remove in movebank as outlier marked positions (or not).

--- RFunction.R
+++ RFunction.R
@@ -1,21 +1,27 @@
 library('move')
 library('foreach')
 
-rFunction <- function(data, maxspeed=NULL)
+rFunction <- function(data, maxspeed=NULL, MBremove=TRUE)
 {
   Sys.setenv(tz="GMT") 
   
-  if (is.null(maxspeed)) logger.info("No maximum speed provided, only manually marked Outliers removed.") else logger.info(paste0("Remove manually marked outliers and positions with maximum speed > ", maxspeed,"m/s"))
+  if (is.null(maxspeed)) 
+  {
+    if (MBremove==TRUE) logger.info("No maximum speed provided, but in Movebank marked Outliers removed.") else logger.info("No maximum speed provided and required to leave Movebank marked Outliers in. Return input data set.")
+  } else 
+  {
+    if (MBremove==TRUE) logger.info(paste0("Remove positions with maximum speed > ", maxspeed,"m/s and in Movebank marked Outliers.")) else logger.info(paste0("Remove positions with maximum speed > ", maxspeed,"m/s, but as requested leave Movebank marked outliers in."))
+  }
   
   data.split <- move::split(data)
   clean <- foreach(datai = data.split) %do% {
     logger.info(namesIndiv(datai))
-    #datai <- datai[(is.na(datai@data$manually_marked_outlier) | datai@data$manually_marked_outlier==""),] #ask sarah about this feature
+    if (MBremove==TRUE) datai <- datai[as.logical(datai$visible)==TRUE,] else datai <- datai #all marked outliers go together in "visible"
     if (!is.null(maxspeed)) datai[speed(datai)<maxspeed,] else datai
   }
   names(clean) <- names(data.split)
 
-  clean_nozero <- clean[unlist(lapply(clean, length) > 1)] #remove list elements of length 1 or 0
+  clean_nozero <- clean[unlist(lapply(clean, length) > 0)] #remove list elements of length 0
   moveStack(clean_nozero)
 }
   
