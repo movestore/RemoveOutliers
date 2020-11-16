118024e6d0bae5695d46e11a72db3479c91278b7
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2020-11-16 10:10:20 +0100

    update code, readme, appspec

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
-# RemoveOutliers
+# Remove Outliers
+MoveApps
+
+Github repository: *github.com/movestore/RemoveOutliers*
 
 ## Description
-Different types of outliers can be removed from a Movement data set: any locations that exceed a user-given speed, that have been marked as outliers in Movebank or that have an unrealistic, future timestamp. 
+Different types of outliers can be removed from a Movement data set: any locations that exceed a user-given speed, that have been marked as outliers in Movebank, that have an unrealistic, future timestamp or that exceed a user-specified error attribute.
 
 ## Documentation
+This App is designed to delete outliers from your data set before any analysis. It thereby uses the Movebank defined outliers, which lead to the variable `visible` being `FALSE`. Furthermore, it allows removing locations to which a user-given speed is exceeded. Also tag specific accuracy variable can be defined and taken out if exceeding a ceratin value. Finally, locations with timestamps that lie in the future can be removed.
+
+### Input data
+moveStack in Movebank format
+
+### Output data
+moveStack in Movebank format
+
+### Artefacts
+none
+
+### Parameters 
+`maxspeed`: Maximum ground speed that the animals are capable of moving. Unit: m/s Example: 50.
+
+`MBremove`: Indication if Movebank defined outliers shall be removed. Checkbox (Yes/No)
+
+`FUTUREremove`: Indication if future timestamps shall be removed. Checkbox (Yes/No)
+
+`accuracy_var`: Name of the data attribute that indicates location errors. Please be very accurate in spelling. Example. "location_error_numerical".
+
+`minaccuracy`: Minimum accuracy that is required for useful data. Exceeding this value leads to location removal. This related tot he user-defined `accuracy_var`. Example: 30
+
+### Null or error handling:
+**Parameter `maxspeed`:** If no maximum speed is given (NULL), then the locations are not filtered for this property. 
+
+**Parameter `MBremove`:** This value can only be Yes or No, so no error or null possible.
+
+**Parameter `FUTUREremove`:** This value can only be Yes or No, so no error or null possible.
+
+**Parameter `accuracy_var`:** If this variable name is not given or does not fit with any of the data attributes, the data will not be filtered for accuracy.
+
+**Parameter `minaccuracy`:** If this value is not given (NULL), the data will not be filtered for accuracy.
+
+**Data:** If no data are retained after all outlier removal, NULL is returned, likely leading to an error.
--- RFunction.R
+++ RFunction.R
@@ -5,12 +5,18 @@ rFunction <- function(data, maxspeed=NULL, MBremove=TRUE, FUTUREremove=TRUE, acc
 {
   Sys.setenv(tz="GMT") 
   
-  if (is.null(maxspeed) & MBremove==FALSE & is.null(accuracy_var)) logger.info("No maximum speed provided, no accuracy variable defined and required to leave Movebank marked Outliers in. Return input data set.")
+  if (accuracy_var %in% names(data)==FALSE)
+  {
+    logger.info("Your defined accuracy variable name does not exist in the data. Please double check. Here set it to NULL")
+    accuracy_var <- NULL
+  }
+  
+  if (is.null(maxspeed) & MBremove==FALSE & FUTUREremove==FALSE & (is.null(accuracy_var) | is.null(minaccuracy))) logger.info("No maximum speed provided, no accuracy variable/minimum accuracy defined and required to leave Movebank marked Outliers and future timestamp locations in. Return input data set.")
   
   if (!is.null(maxspeed)) logger.info(paste0("Remove positions with maximum speed > ", maxspeed,"m/s"))
   if (MBremove==TRUE) logger.info("In Movebank marked outliers will be removed.")
-  if (!is.null(accuracy_var)) logger.info(paste("Remove positions with high location error:",accuracy_var,">",minaccuracy))
-  
+  if (!is.null(accuracy_var) & !is.null(minaccuracy)) logger.info(paste("Remove positions with high location error:",accuracy_var,">",minaccuracy))
+
   data.split <- move::split(data)
   clean <- foreach(datai = data.split) %do% {
     logger.info(namesIndiv(datai))
@@ -20,24 +26,25 @@ rFunction <- function(data, maxspeed=NULL, MBremove=TRUE, FUTUREremove=TRUE, acc
       if (length(ix)>0) 
       {
         logger.info(paste("For this animals",length(ix),"in Movebank marked outliers were removed for animal",namesIndiv(datai)))
-        datai[-ix,]
-      } else datai
-    } else datai 
+        datai <- datai[-ix,]
+      }
+    }
     if (!is.null(accuracy_var) & !is.null(minaccuracy)) 
       {
         ixA <- which(datai@data[,accuracy_var]>=as.numeric(minaccuracy))
         if (length(ixA)>0)
         {
           logger.info(paste("For this animal",length(ixA),"positions with high errors are removed:",accuracy_var,">",minaccuracy))
-          datai[-ixA,] 
-        } else datai
-      } else datai
+          datai <- datai[-ixA,] 
+        }
+      }
     if (!is.null(maxspeed)) 
       {
         ixS <- which(speed(datai)>maxspeed)  
         logger.info(paste("For this animal",length(ixS),"positions are removed due to between location speeds >",maxspeed,"m/s"))
-        datai[-ixS,]
-      } else datai
+        datai <- datai[-ixS,]
+    }
+    datai
   }
   names(clean) <- names(data.split) #clean is still list of move objects
 
